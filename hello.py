f26f6ff2fe43d8d64e4108e2f7c5591e5a85a6ce
mazwaay <mazwaay@gmail.com>
2024-03-28 01:32:25 +0700

    update scripts

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
-print ("hello world")
-print("test2")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+# handle selenium close automatic after test run
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://stage.mobipaid.com/en/register")
+print("Page title is:" + driver.title)
+
+time.sleep (5)
+driver.quit()
